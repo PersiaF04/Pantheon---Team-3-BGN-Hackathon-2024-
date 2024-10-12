faa794e84922ec4f606cae6425b44b84a62b0d8d
0xUgochukwu <0xUgochukwu@gmail.com>
2024-10-12 14:29:21 +0100

    basic setup

---
+++ mern/server/src/config/env.js
@@ -0,0 +1,5 @@
+import dotenv from "dotenv";
+
+dotenv.config({ path: "config.env" });
+
+export const PORT = process.env.PORT || 8080;
---
+++ mern/server/src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import logger from "morgan";
+
+import * as config from "./config/env.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(logger("tiny"));
+
+// start the Express server
+app.listen(config.PORT, () => {
+  console.log(`Server listening on port ${config.PORT}`);
+});
