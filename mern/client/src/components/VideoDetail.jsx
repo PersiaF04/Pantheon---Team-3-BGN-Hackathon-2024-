13e1b6557b6091c10ea120c3dee18a90cd583cc9
0xUgochukwu <0xUgochukwu@gmail.com>
2024-10-14 00:43:41 +0100

    video detail fix

--- mern/client/src/components/VideoDetail.jsx
+++ mern/client/src/components/VideoDetail.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
-import Banner from './banner';
-import Footer from './Footer';
-import Widgets from './Widget';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import Banner from "./banner";
+import Footer from "./Footer";
+import Widgets from "./Widget";
 const VideoDetail = () => {
   const { id } = useParams(); // Get the video ID from the URL
   const [video, setVideo] = useState(null);
@@ -11,39 +11,42 @@ const VideoDetail = () => {
   useEffect(() => {
     const fetchVideo = async () => {
       try {
-        const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/videos/${id}`);
-        setVideo(response.data);
+        console.log("test ID", id);
+        const response = await axios.get(
+          `${import.meta.env.VITE_BASE_URL}/videos/${id}`,
+        );
+        setVideo(response.data.video);
+        console.log("test", video);
       } catch (error) {
         console.error("Fetch video details failed:", error);
       }
     };
 
-    fetchVideo();
+    if (id) {
+      fetchVideo();
+    }
   }, [id]);
 
-  if (!video) return <div>Loading...</div>;
-
   return (
-    <div className="flex flex-col min-h-screen"> 
+    <div className="flex flex-col min-h-screen">
       <Banner />
-      
-      <div className="flex-grow p-4"> 
-        <h1 className="text-2xl font-bold mb-4">{video.title}</h1>
-        
-       
+
+      <div className="flex-grow p-4">
+        <h1 className="text-2xl font-bold mb-4">{video?.title}</h1>
+
         <div className="my-4 w-full max-w-4xl mx-auto aspect-video">
-          <iframe 
-            src={video.url} 
-            title={video.title} 
-            width="100%" 
-            height="100%" 
-            frameBorder="0" 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+          <iframe
+            src={`https://www.youtube.com/embed/${video?.url.split("=")[1]}`}
+            title={video?.title}
+            width="100%"
+            height="100%"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe>
         </div>
-        
-        <p className="text-gray-700 mt-4">{video.description}</p>
+
+        <p className="text-gray-700 mt-4">{video?.description}</p>
       </div>
       <Widgets />
       <Footer />
